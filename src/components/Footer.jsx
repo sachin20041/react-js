787bd827a409f63f3ba9a6ecd2753d80ce79d0a1
sachin20041 <sachinsakthivel8@gmail.com>
2023-11-07 19:38:32 +0530

    components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer= () =>{
+    return(
+        <div>
+           
+           <hr></hr>
+           <h2><center><b>727722euit154@skcet.ac.in</b></center></h2>
+           <hr></hr>
+
+        </div>
+    );
+};
+
+export default Footer;
